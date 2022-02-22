4f0373d3b24214b9cfb5a1aaea7949dec12f8e3a
dawon0304 <dawon0304@naver.com>
2022-02-22 18:42:17 +0900

    cleanup function

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("i run only once");
+function Hello() {
   useEffect(() => {
-    console.log("i run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("i run when 'counter' changes.")
-  }, [counter]);
-    useEffect(() => {
-    console.log("i run when keyword & counter change.")
-  }, [keyword,counter]);
+    console.log("created");
+    return() => console.log("destroyed :( ");
+  }, [])
+  return <h1>Hello</h1>;
+}
 
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);  
+  /*setShowing을 통해 이전 value를 받아온 다음에, 그 value의 반댓값을 return 할 것이다. */
+  
   return (
     <div>
-      <input vaue={keyword} onChange={onChange} type="text" placeholder="Search here.."></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
-      </div>
+      {showing ? <Hello /> : null } 
+      {/* 만약 showing이 true라면 hello component를 render할꺼야. 아니라면 아무것도 안보여줄꺼야 */}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
   );
 }
 
